dd42a13691a618b9eda18383bbb1e03dbe1d7cf4
Rahul <dhiman7571@gmail.com>
2022-12-28 15:10:56 +0530

    update mobile feature

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-function myFunction() {
-  var dots = document.getElementsByClassName("arrow");
-  var moreText = document.getElementsByClassName("more");
-  var btnText = document.getElementById("myBtn");
+// element.addEventListener(click, ()))
+//  e / event -> e - > represent elemtn on which event happen
+let acc = document.getElementsByClassName("p");
+let i;
 
-  if (arrow.style.display === "none") {
-    arrow.style.display = "inline";
-    btnText.innerHTML = "arrow";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "more";
-    moreText.style.display = "inline";
-  }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+    this.classList.remove("none");
+
+    /* Toggle between hiding and showing the active panel */
+    let more = this.nextElementSibling;
+    if (more.style.display === "none") {
+      more.style.display = "block";
+    } else {
+      more.style.display = "none";
+    }
+  });
 }
+// const accordion = document.querySelectorAll(".p");
+
+// for (p of accordion) {
+//   p.addEventListener("click", function () {
+//     this.classList.toggle("active");
+
+//     let more = this.nextElementSibling;
+//     if (more.classList.contains("show")) {
+//       more.classList.remove("show");
+//     } else {
+//       more.classList.add("show");
+//     }
+
+//     for (p of accordion) {
+//       if (p != this) {
+//         p.classList.remove("active");
+//         p.nextElementSibling.classList.remove("show");
+//       }
+//     }
+//   });
+// }
